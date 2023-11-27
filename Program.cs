7719bd12ba61cde1539424d66222a0aafa81ba9e
Aitugan Saktaganov <aitugan@mail.ru>
2023-11-28 01:20:32 +0600

    Solution of the training's main block's test in C# language

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Get3LetterElements
+{
+    public static void Main() {
+        string[] stringArray = ["Hello", "2", "world", ":-)"];                       // ["2", ":-)"]
+        // string[] strings = ["1234", "1567", "-2", "computer science"];           // ["-2"]
+        // string[] strings = ["Russia", "Denmark", "Kazan"];                          // []
+
+        int count = 0;
+
+        for (int i = 0; i < stringArray.Length; i++) {
+            if (stringArray[i].Length <= 3) {
+                string swap = stringArray[count];
+                stringArray[count++] = stringArray[i];
+                stringArray[i] = swap;
+            }
+        }
+
+        string[] threeLetterStrings = new string[count];
+        Array.Copy(stringArray, threeLetterStrings, count);
+
+        Console.WriteLine("[{0}]", string.Join(", ", threeLetterStrings));
+    }
+}
